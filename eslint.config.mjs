d5f678471131ef815ad5080fb6c21c69deafd76a
awardquick <aaron@goodparty.org>
2025-12-03 18:47:46 -0500

    working on setting up prisma schema

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
+ 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
